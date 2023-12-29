0e549957be17814b2c6f8a37760caa65bed54b66
harry <harrytsao12345@gmail.com>
2023-12-29 11:47:18 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Data Science and Informatics</h1>
+      <h1 className="head-text text-left">Data Science and Informatics</h1>
     </main>
   )
 }
