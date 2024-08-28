ea4681c76e4a859549c64ed9d03410493ab2369d
Harry <harrytsao12345@gmail.com>
2024-08-27 23:38:50 -0400

    initial commit redesign

--- constants/index.ts
+++
@@ -1,9 +0,0 @@
-// NAVIGATION
-export const NAV_LINKS = [
-    { href: '/', key: 'about', label: 'About'},
-    { href: '/workshops', key: 'workshops', label: 'Workshops'},
-    { href: '/officers', key: 'officers', label: 'Officers'},
-    { href: '/alumni', key: 'alumni', label: 'Alumni'},
-    { href: '/contact', key: 'contact', label: 'Contact'},
-
-]
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
