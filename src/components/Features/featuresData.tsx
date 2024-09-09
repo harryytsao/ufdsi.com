d8510821e2fbf4b669be8f587a8bf48f610fc16d
Harry <harrytsao12345@gmail.com>
2024-09-09 17:58:06 -0400

    typo

--- src/components/Features/featuresData.tsx
+++ src/components/Features/featuresData.tsx
@@ -12,7 +12,7 @@ const featuresData: Feature[] = [
         <path d="M23.2222 0C22.6699 0 22.2222 0.447715 22.2222 1V12.3333C22.2222 12.8856 22.6699 13.3333 23.2222 13.3333H39C39.5523 13.3333 40 12.8856 40 12.3333V0.999999C40 0.447714 39.5523 0 39 0H23.2222ZM0 39C0 39.5523 0.447715 40 1 40H16.7778C17.3301 40 17.7778 39.5523 17.7778 39V27.6667C17.7778 27.1144 17.3301 26.6667 16.7778 26.6667H1C0.447716 26.6667 0 27.1144 0 27.6667V39ZM0 21.2222C0 21.7745 0.447715 22.2222 1 22.2222H16.7778C17.3301 22.2222 17.7778 21.7745 17.7778 21.2222V0.999999C17.7778 0.447714 17.3301 0 16.7778 0H1C0.447716 0 0 0.447715 0 1V21.2222Z" />
       </svg>
     ),
-    title: "Techincal Workshops",
+    title: "Technical Workshops",
     paragraph:
       "We hold workshops and to provide the resources to teach students valuable skills in Data Science that can further students career in research and industry",
   },
